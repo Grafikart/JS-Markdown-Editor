38e90497978f29a6285e4109c235097d8413f538
EnderMC <55290516+endermctv@users.noreply.github.com>
2021-02-14 20:53:39 +0100

    Update upload.php

--- app/upload.php
+++ app/upload.php
@@ -1,18 +1,16 @@
 <?php
-/**
- * This is for demo purpose, copy this code on your website will make it explode !
-**/s
+
+// This is for demo purpose, copy this code on your website will make it explode!
 
 header('Content-Type: application/json');
 $file = '1.jpg';
 move_uploaded_file($_FILES['image']['tmp_name'], $file);
-$results = array(
+$results = [
   'id' => basename($file),
   'url'=> $file
-);
+];
 
-/**
- * Your upload must return a json {id: XX, url: full_image_url} or an string error with a status code != 200
- */
+// Your upload must return a json {id: XX, url: full_image_url} or an string error with a status code != 200
+
 header('Content-Type: application/json');
 echo json_encode($results);
